a4df27c9d2a9f709a375543895b70b2cf4e303b1
Stewart Powell <sgpowell007@gmail.com>
2021-02-19 23:08:40 -0800

    updated for Flixster part two with tab view and collection view

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Stewart Powell on 2/19/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
